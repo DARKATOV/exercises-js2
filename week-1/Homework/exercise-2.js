9e0322e69159f583dcf2538061e13981e900091d
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-03-20 06:51:38 +0100

    finish

--- week-1/Homework/exercise-2.js
+++ week-1/Homework/exercise-2.js
@@ -8,12 +8,19 @@ let person = {
     name: "Alice",
     friends: ["John", "Nina"],
   };
-  
+    
+// person.friends [2]='Bob';
+// console.log(person.friends);
+
+function personmakeFriend (newFriend){
+  person.friends[person.friends.length] = newFriend;
+}
+
   /*
   DO NOT EDIT ANYTHING BELOW THIS LINE
   */
   
-  person.makeFriend("Bob");
+  personmakeFriend('Bob'); // aqui tenia un punto no entiendo la razon , pero no me dejaba colocarlo asi en la funcion 
   
   console.log(
     `Expected result: 'John,Nina,Bob'. Actual result: ${person.friends}`

6e76ce4dcbcce42dab3288483c27d3b4898c69e1
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-04-09 13:07:21 +0200

    finish

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,13 +1,19 @@
 // Write your code here
 
+const autoback = document.getElementById("autoback");
 const back = document.getElementById("back");
+const stopButton = document.getElementById("stop");
 const forward = document.getElementById("forward");
+const autoforward = document.getElementById("autoforward");
 const img = document.getElementById("img");
 const data = ['1.jpg','2.jpg','3.jpg','4.jpg'];
 let counterImg = 0;
 
+autoforward.addEventListener("click", autoNextImg);
 forward.addEventListener("click", nextImg);
+stopButton.addEventListener("click", stopAuto);
 back.addEventListener("click", previousImg);
+autoback.addEventListener("click", autoPreviousImg);
 
 function nextImg(){
     if ( counterImg <= data.length-2 ){
@@ -31,3 +37,23 @@ function previousImg(){
         return counterImg = data.length-1;
     }
 };
+
+let intervalNext = -1;
+let intervalPrevious = -1;
+
+function autoNextImg(){
+    clearInterval(intervalNext);
+    clearInterval(intervalPrevious);
+    intervalNext = setInterval(nextImg,1000);
+}
+
+function autoPreviousImg(){
+    clearInterval(intervalNext);
+    clearInterval(intervalPrevious);
+    intervalPrevious = setInterval(previousImg,1000);
+}
+
+function stopAuto(){
+    clearInterval(intervalNext);
+    clearInterval(intervalPrevious);
+}
